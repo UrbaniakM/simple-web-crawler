0bbf304bf3ef55a7bded5a530871cf97e659a6fe
UrbaniakM <31575249+UrbaniakM@users.noreply.github.com>
2018-04-09 02:46:53 +0200

    -add retriving name, price, abv and size from hrefs

--- src/Crawl.jsx
+++ src/Crawl.jsx
@@ -2,11 +2,11 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 class Crawl {
-
-    crawlWebpage = () => {
+    
+    processProduct = (product_url_address) => {
         const request_promise = require('request-promise');
         request_promise({
-            uri: 'https://domwina.pl/pol_m_Oferta-100.html',
+            uri: product_url_address,
             method: 'GET',
             resolveWithFullResponse: true,
             headers: {
@@ -15,15 +15,40 @@ class Crawl {
                 'User-Agent': 'Request-Promise'
             }
         })
-        .then(function(response) {
+        .then((response) => {
             const dom = new JSDOM(response.body, { includeNodeLocations: true });
             const document = dom.window.document;
-            const body = document.body;
-            const hrefsAll = [...document.querySelectorAll(".product-name")];
-            console.log(hrefsAll);
+            const name = document.querySelector(".product-name *");
+            const price = document.querySelector(".price-including-tax .price");
+            const abv = document.querySelector(".abv span");
+            const size = document.querySelector(".bottleSize span");
+            const result = [name.textContent, price.textContent, abv.textContent, size.textContent];
+            console.log(result);
+            return result;
+        })
+        .catch(function(err) {
+            console.log("Error: " + err);
+        });
+    }
+
+    crawlWebpage = () => {
+        const request_promise = require('request-promise');
+        request_promise({
+            uri: 'https://www.whiskyshop.com/single-malt-scotch-whisky', // TODO: https://www.whiskyshop.com/single-malt-scotch-whisky?p=1 -> ?p=5
+            method: 'GET',
+            resolveWithFullResponse: true,
+            headers: {
+                'Accept': '*/*',
+                'Content-Type': 'text/plain',
+                'User-Agent': 'Request-Promise'
+            }
+        })
+        .then((response) => {
+            const dom = new JSDOM(response.body, { includeNodeLocations: true });
+            const document = dom.window.document;
+            const hrefsAll = [...document.querySelectorAll(".product-item-information > a")];
             hrefsAll.map(hrefs => {
-                console.log(dom.nodeLocation(hrefs));
-                console.log(hrefs.href);
+                console.log(this.processProduct(hrefs.href));
             });
         })
         .catch(function(err) {
